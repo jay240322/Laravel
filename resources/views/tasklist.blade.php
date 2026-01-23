5d4a7755ddb3b8db11689bb09aa602549597056b
jay patel <patel.jay.brij03@gmail.com>
2026-01-23 17:07:21 +0530

    change

--- resources/views/tasklist.blade.php
+++ resources/views/tasklist.blade.php
@@ -2,14 +2,18 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width, initial-scale=1.0">
     <title>Document</title>
 </head>
 <body>
-    <ul>
-        @foreach($task as $task)
-           <li>{{$task}}</li>
-        @endforeach
-    </ul>
+    @foreach($tasks as $task)
+    <li>{{$task}}</li>
+    @endforeach
+
+    {{count($stack)}}
+
+    @foreach($stack as $s)
+    <li>{{$s}}</li>
+    @endforeach
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -6,66 +6,101 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/pr5', function(){
-    return "<h1>Welcome </h1>";
+// Route::get('/pr5', function(){
+//     return "<h1>Welcome </h1>";
+// });
+
+// Route::get('/pr6',function(){
+//     return[
+//         ['id'=>1,'name'=>'john'],
+//         ['id'=>2,'name'=>'doe'],
+//     ];
+// });
+
+// Route::get('/pr7/{name}',function($name){
+//     echo $name;
+// });
+
+// Route::get('/pr8/{name}/{id}',function($name,$id){
+//     echo $name.",".$id;
+// });
+
+// Route::get('/pr9',function(){
+//     $task = ['task1','task2','task3'];
+//     return view('tasklist',compact('task'));
+// });
+
+// Route::get('/pr11',function(){
+//     return view('profile') -> with('name','john doe');
+// });
+
+// Route::get('/pr12/{name}',function($name){
+//     $data = 'Lcca';
+//     return view('login12') -> with(compact('name','data'));
+// });
+
+// Route::view('/pr13','home');
+
+
+// Route::view('/pr14','dashboard');
+// Route::view('/register','register');
+// Route::view('/login','login');
+
+
+// Route::get('/pro16',function(){
+//     $tasks = ['study','play','cook'];
+//     return view('task',compact('tasks'));
+// });
+
+// Route::get('/pro17',function(){
+//     $task = (object)[
+//         'title' => 'my first task',
+//         'completed' => true  //or write false
+//     ];
+
+//     return view('checktask',compact('task'));
+// });
+
+// Route::get('/pro18',function(){
+
+//     $tasks = [
+//         ['title'=> 'study','completed'=> true],
+//         ['title'=> 'code','completed'=>false],
+//     ];
+//     return view('emptytask',compact('tasks'));
+// });
+
+
+
+Route::get('/pro1',function(){
+    echo "hello world";
 });
 
-Route::get('/pr6',function(){
-    return[
-        ['id'=>1,'name'=>'john'],
-        ['id'=>2,'name'=>'doe'],
-    ];
+Route::get('/pro6',function(){
+  return[
+    ['id'=>1,'name'=>'LCCA'],
+    ['id'=>2,'name'=>'BCA'],
+  ];
 });
 
-Route::get('/pr7/{name}',function($name){
-    echo $name;
+Route::get('/pro7/{name}/{proirity}',function($name,$proirity){
+ 
+    echo $name.",".$proirity;
 });
 
-Route::get('/pr8/{name}/{id}',function($name,$id){
-    echo $name.",".$id;
+Route::get('/pro9',function(){
+    $tasks=['play','study','code'];
+    return view('tasklist',compact('tasks'));
 });
 
-Route::get('/pr9',function(){
-    $task = ['task1','task2','task3'];
-    return view('tasklist',compact('task'));
+Route::get('/pro10',function(){
+      $tasks=['play','study','code'];
+    
+    $stack=['A','B','C'];
+    return view('tasklist',compact('tasks','stack'));
 });
 
-Route::get('/pr11',function(){
-    return view('profile') -> with('name','john doe');
+Route::get('/pro11',function(){
+    return view('profile')->with('name','LCCA');
 });
 
-Route::get('/pr12/{name}',function($name){
-    $data = 'Lcca';
-    return view('login12') -> with(compact('name','data'));
-});
-
-Route::view('/pr13','home');
-
-
-Route::view('/pr14','dashboard');
-Route::view('/register','register');
-Route::view('/login','login');
-
-
-Route::get('/pro16',function(){
-    $tasks = ['study','play','cook'];
-    return view('task',compact('tasks'));
-});
-
-Route::get('/pro17',function(){
-    $task = (object)[
-        'title' => 'my first task',
-        'completed' => true  //or write false
-    ];
-
-    return view('checktask',compact('task'));
-});
-
-Route::get('/pro18',function(){
-
-    $tasks = [
-        ['title'=> 'study','completed'=> true],
-        ['title'=> 'code','completed'=>false],
-    ];
-    return view('emptytask',compact('tasks'));
-});
